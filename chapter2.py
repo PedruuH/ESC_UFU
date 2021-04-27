329e972d73cf48bf66fb1dccbeacf75f0f75a2c2
PedruuH <pedro.inventador@hotmail.com>
2021-04-26 21:50:37 -0300

    final ALU

--- chapter2.py
+++ chapter2.py
@@ -77,47 +77,49 @@ Inc16.save()
 
 Alu = Circuit('Alu', lbs('x', 16) + lbs('y', 16) + ['zx', 'nx', 'zy', 'ny', 'f', 'no'], lbs('out', 16) + ['zr', 'ng'])
 Alu.add_components(
-    (Library.load('Mux16'), 6), #Mux de 0 a 6
-    (Library.load('Not16'), 3), #Not16 7, 8 e 9
-    Library.load('And16'), #And 10
-    Library.load('Add16'), #Add 11
-    Nor16way #Nor 12
+    (Library.load('Mux16'), 6), #Mux de 0 a 5
+    (Library.load('Not16'), 3), #Not16 6, 7 e 8
+    Library.load('And16'), #And 9
+    Library.load('Add16'), #Add 10
+    Nor16way #Nor 11
 )
-Alu.set_as_input(1, 'sel', 'zx')
-Alu.set_as_input(2, 'sel', 'nx')
-Alu.set_as_input(3, 'sel', 'zy')
-Alu.set_as_input(4, 'sel', 'ny')
-Alu.set_as_input(5, 'sel', 'f')
-Alu.set_as_input(6, 'sel', 'no')
-
+Alu.set_as_input(0, 'sel', 'zx')
+Alu.set_as_input(1, 'sel', 'nx')
+Alu.set_as_input(2, 'sel', 'zy')
+Alu.set_as_input(3, 'sel', 'ny')
+Alu.set_as_input(4, 'sel', 'f')
+Alu.set_as_input(5, 'sel', 'no')
+Alu.set_as_output(11, 'out', 'zr' )
+Alu.set_as_output(5, 'out15', 'ng' )
 for i in range(16):
-    Alu.set_as_input(1, f'a{i}',f'x{i}')
-    #Alu.set_as_input(1, f'b{i}',  )
-    Alu.set_as_input(3, f'a{i}',f'y{i}')
-    #Alu.set_as_input(3, f'b{i}', 0)
-    Alu.connect(1, f'out{i}', 7, f'in{i}')
-    Alu.connect(1, f'out{i}', 2, f'a{i}')
-    Alu.connect(3, f'out{i}', 8, f'in{i}')
-    Alu.connect(3, f'out{i}', 4, f'a{i}')
-    Alu.connect(7, f'out{i}', 2, f'b{i}')
-    Alu.connect(8, f'out{i}', 4, f'b{i}')
-    Alu.connect(2, f'out{i}', 10, f'a{i}')
-    Alu.connect(4, f'out{i}', 10, f'b{i}')
-    Alu.connect(2, f'out{i}', 11, f'a{i}')
-    Alu.connect(4, f'out{i}', 11, f'b{i}')
-    Alu.connect(10, f'out{i}', 5, f'a{i}')
-    Alu.connect(11, f'out{i}', 5, f'b{i}')
-    Alu.connect(5, f'out{i}', 6, f'a{i}')
-    Alu.connect(5, f'out{i}', 9, f'in{i}')
-    Alu.connect(9, f'out{i}', 6, f'b{i}')
-    Alu.connect(6, f'out{i}', 12, f'in{i}')
-    Alu.set_as_output(6, f'out{i}', f'out{i}' )
-Alu.set_as_output(12, 'out', 'zr' )
+    Alu.set_as_output(5, f'out{i}', f'out{i}' )
+    Alu.set_as_input(0, f'a{i}',f'x{i}')
+    Alu.set_as_input(2, f'a{i}',f'y{i}')
+    Alu.set_low_input(0,f'b{i}')
+    Alu.set_low_input(2,f'b{i}')
+    Alu.connect(0, f'out{i}', 6, f'in{i}')
+    Alu.connect(0, f'out{i}', 1, f'a{i}')
+    Alu.connect(6, f'out{i}', 1, f'b{i}')
+    Alu.connect(2, f'out{i}', 7, f'in{i}')
+    Alu.connect(2, f'out{i}', 3, f'a{i}')
+    Alu.connect(7, f'out{i}', 3, f'b{i}')       
+    Alu.connect(1, f'out{i}', 9, f'a{i}')
+    Alu.connect(1, f'out{i}', 10, f'a{i}')
+    Alu.connect(3, f'out{i}', 9, f'b{i}')
+    Alu.connect(3, f'out{i}', 10, f'b{i}')    
+    Alu.connect(9, f'out{i}', 4, f'a{i}')
+    Alu.connect(10, f'out{i}', 4, f'b{i}')
+    Alu.connect(4, f'out{i}', 5, f'a{i}')
+    Alu.connect(4, f'out{i}', 8, f'in{i}')
+    Alu.connect(8, f'out{i}', 5, f'b{i}')
+    Alu.connect(5, f'out{i}', 11, f'in{i}')
+Alu.save()
+  
 
-Alu.test_arithm(msg="x+y", 
-      x=10, y=12, zx=0, nx=0, zy=0, ny=0, f=1, no=0, 
+Alu.test_arithm(msg="x&y", 
+      x=10, y=12, zx=0, nx=0, zy=0, ny=0, f=0, no=0, 
       label_display_order=(
           ['zx', 'nx', 'zy', 'ny', 'f', 'no'] + lbs('x', 16) + lbs('y', 16), 
-          ['zr', 'ng'] + lbs('out', 16)
+          ['zr', 'ng']+lbs('out', 16)
       ), 
       unsigned=['zx', 'nx', 'zy', 'ny', 'f', 'no', 'zr', 'ng'])
